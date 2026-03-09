c1e0ec33ca2b87b18bfd61116e36b0ce5c5e92fa
emillward2004-cmyk <emillward2004@gmail.com>
2026-03-09 21:37:18 +0000

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,22 @@
-.
+const DB_NAME = "KwikfitPartsDB";
+const STORE_NAME = "parts";
+
+export function openDB() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, 1);
+
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+
+      const store = db.createObjectStore(STORE_NAME, {
+        keyPath: "id"
+      });
+
+      store.createIndex("category", "category");
+      store.createIndex("partName", "partName");
+    };
+
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+}
