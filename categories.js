13e5adeabafeac8a58c3e9eab2c907b7fc27b001
emillward2004-cmyk <emillward2004@gmail.com>
2026-03-09 21:36:34 +0000

    Update categories.js

--- categories.js
+++ categories.js
@@ -1,1 +1,12 @@
-.
+export const CATEGORIES = [
+  "Servicing",
+  "Brakes",
+  "Suspension",
+  "Steering",
+  "Engine",
+  "Cooling",
+  "Electrical",
+  "Exhaust",
+  "Transmission",
+  "Misc"
+];
