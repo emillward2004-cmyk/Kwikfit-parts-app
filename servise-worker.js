59aa82ba6a887a4653d649291daa88412ddb1ebd
emillward2004-cmyk <emillward2004@gmail.com>
2026-03-09 21:39:35 +0000

    Update servise-worker.js

--- servise-worker.js
+++ servise-worker.js
@@ -1,1 +1,12 @@
-.
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("parts-app").then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css",
+        "/app.js"
+      ])
+    )
+  );
+});
